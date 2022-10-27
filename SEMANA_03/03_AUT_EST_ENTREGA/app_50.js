ac8fc3881b453fd22fd0a8983dcafd56a79bb4ae
NDPRei <110630356+NDPRei@users.noreply.github.com>
2022-10-27 12:47:10 -0300

    Add files via upload

---
+++ SEMANA_03/03_AUT_EST_ENTREGA/app_50.js
@@ -0,0 +1,98 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = 'curriculo.db'; //use o nome que você achar melhor para o banco de dados
+
+app.use(express.json());
+app.get('/Formacao', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT Curso, Data_ini, Descricao, Data_fim, idPerfil FROM Formacao ORDER BY Data_fim DESC';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+app.use(express.json());
+app.get('/Experiencia', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT Nome_da_empresa, Data_ini, Cargo, Data_fim, idPerfil FROM Experiencia ORDER BY Data_fim DESC';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+app.use(express.json());
+app.get('/Realizacoes', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT Descricao, Data, idPerfil FROM Realizacoes ORDER BY Data DESC';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+app.use(express.json());
+app.get('/Perfil', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT Foto, Nome, Cargo, Endereco, Email, Contato, ID FROM Perfil ORDER BY Nome DESC';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+app.use(express.json());
+app.get('/Habilidades', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT Habilidades, ID FROM Perfil ORDER BY Habilidades DESC';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+app.use(express.json());
+app.get('/Personalidade', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT Descricao, ID FROM Perfil ORDER BY Descricao DESC';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+app.listen(3000)
